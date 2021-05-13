8896beac0d9f65e8fb30fa781730e9a5d6c7c8a5
gunstonej <gunstone.john@gmail.com>
2021-05-13 03:10:19 +0100

    added constants

--- ipyrun/constants.py
+++ ipyrun/constants.py
@@ -2,8 +2,10 @@ import os
 # from ipyrun.constants import BUTTON_WIDTH_MIN, BUTTON_WIDTH_MEDIUM, FDIR_ROOT_EXAMPLE, FPTH_SCRIPT_EXAMPLE, FDIR_APP_EXAMPLE
 FDIR_PACKAGE = os.path.realpath(os.path.join(os.path.dirname(__file__),'..'))
 FDIR_ROOT_EXAMPLE = os.path.join(FDIR_PACKAGE,'examples','J0000')
-FDIR_APP_EXAMPLE = os.path.join(FDIR_ROOT_EXAMPLE,'Automation','ExampleApp')
-FPTH_SCRIPT_EXAMPLE = os.path.join(FDIR_PACKAGE,'examples','scripts','expansion_vessel_sizing.py')
+FDIR_APP_EXAMPLE = os.path.join(FDIR_ROOT_EXAMPLE,'test_appdir')
+FPTH_SCRIPT_EXAMPLE = os.path.join(FDIR_PACKAGE,'test_scripts','expansion_vessel_sizing.py')
+FPTH_SCRIPT_EXAMPLE_CSV = os.path.join(FDIR_PACKAGE,'test_scripts','line_graph.py')
+FPTH_RUNAPP_HELP = os.path.join(FDIR_PACKAGE, 'ipyrun', 'images', 'RunApp.png')
 
 BUTTON_WIDTH_MIN = '41px'
 BUTTON_WIDTH_MEDIUM='90px'
