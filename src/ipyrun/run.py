73d4d202cd8e66bdd5207cd4c796793348803ace
john <j.gunstone@maxfordham.com>
2022-01-21 18:02:19 +0000

    major update. 1st working version of RunApp and BatchApp. needs tidying

--- src/ipyrun/run.py
+++ src/ipyrun/run.py
@@ -14,13 +14,13 @@
 
 # %%
 """
-attaches genuine functionality onto the datastructures / UI elements defined in ui_*.py
+A configuration of the ipyrun App for running shell python scripts. 
 """
 # %run __init__.py
 # %load_ext lab_black
 
-# %%
 import io
+import shutil
 import pandas as pd
 from IPython.display import (
     # update_display,
@@ -39,10 +39,12 @@ import importlib.util
 from halo import HaloNotebook
 import pathlib
 import typing
-from typing import Optional, List, Dict, Type, Optional
+from typing import Optional, List, Dict, Type, Optional, Union
 from pydantic.dataclasses import dataclass
 from pydantic import BaseModel, validator, Field
+from ipyrun.basemodel import BaseModel
 from jinja2 import Template
+from enum import Enum, IntEnum
 
 import plotly.io as pio
 import plotly.graph_objects as go
@@ -74,8 +76,8 @@ from ipyrun.constants import (
 test_constants = load_test_constants()
 
 from ipyrun.ui_run import *
-from enum import Enum, IntEnum
 from ipyrun.ui_run import RunActionsUi, RunUi, RunUiConfig
+from ipyrun.ui_add import AddRun
 from ipyrun.schema_actions import RunActions, BatchActions
 from ipyrun.schema_config_runshell import ConfigActionsShell, DefaultConfigActionsShell, DisplayfileDefinition, FiletypeEnum, create_displayfile_renderer
 
@@ -85,82 +87,12 @@ def get_mfuser_initials():
 
 
 # %%
-# template run action callables
 def check(config: ConfigActionsShell):
     config.in_batch = True
-    
+
 def uncheck(config: ConfigActionsShell):
     config.in_batch = False
-    
-def show_files(fpths, class_displayfiles=DisplayFiles, kwargs_displayfiles={}):
-    return class_displayfiles([f for f in fpths], **kwargs_displayfiles)
 
-def update_DisplayFiles(config, app):
-    for d in config.displayfile_definitions: 
-        user_file_renderers.update(create_displayfile_renderer(d, fn_onsave=app._update_status))    
-    return functools.partial(DisplayFiles, user_file_renderers=user_file_renderers)
-
-def run_shell(shell: str, cls=None): #
-    """
-    cmd: str, cls=None
-    """
-    shell = shell.split(" ")
-    pr = """  
-    """.join(shell)
-    display(Markdown(f"{pr}"))
-    spinner = HaloNotebook(animation="marquee", text="Running", spinner="dots")
-    try:
-        spinner.start()
-        save = sys.stdout
-        sys.stdout = io.StringIO()
-        proc = subprocess.Popen(shell)
-        proc.wait()
-        in_stdout = sys.stdout.getvalue()
-        sys.stdout = save
-        display(in_stdout)
-        spinner.succeed("Finished")
-    except subprocess.CalledProcessError as e:
-        spinner.fail("Error with Process")
-    cls._update_status()
-    
-def help_config_show(cls=None):
-    return display_pydantic_json(cls.config)
-
-def fn_buildrunactions(config: ConfigActionsShell, cls=None) -> RunActions:  # fn_onsave
-    # create custom Displayfile 
-    user_file_renderers = {}
-    for d in config.displayfile_definitions: 
-        user_file_renderers.update(create_displayfile_renderer(d, fn_onsave=cls._update_status))    
-    cls_display = functools.partial(DisplayFiles, user_file_renderers=user_file_renderers)
-    
-    run_actions = RunActions(check=functools.partial(check, config),
-                             uncheck=functools.partial(uncheck, config),
-                             get_status=cls._update_status,
-                             help_run_show=functools.partial(show_files, 
-                                                           [config.fpth_script],
-                                                           class_displayfiles=cls_display,
-                                                           kwargs_displayfiles={'auto_open':True}),
-                             help_config_show=help_config_show,
-                             inputs_show=functools.partial(show_files, 
-                                                           config.fpths_inputs,
-                                                           class_displayfiles=cls_display,
-                                                           kwargs_displayfiles=config.displayfile_inputs_kwargs),
-                             outputs_show=functools.partial(show_files, 
-                                                           config.fpths_outputs,
-                                                           class_displayfiles=cls_display,
-                                                           kwargs_displayfiles=config.displayfile_outputs_kwargs),
-                             run=functools.partial(run_shell,config.shell)
-                            )
-    return run_actions
-
-
-# %%
-def check(config: ConfigActionsShell):
-    config.in_batch = True
-    
-def uncheck(config: ConfigActionsShell):
-    config.in_batch = False
-    
 def show_files(fpths, class_displayfiles=DisplayFiles, kwargs_displayfiles={}):
     return class_displayfiles([f for f in fpths], **kwargs_displayfiles)
 
@@ -222,7 +154,7 @@ class RunShellActions(RunActions):
     
     @validator("help_config_show", always=True)
     def _help_config_show(cls, v, values):
-        return functools.partial(display_pydantic_json, values["config"])
+        return functools.partial(display_pydantic_json, values["config"], as_yaml=True)
     
     @validator("inputs_show", always=True)
     def _inputs_show(cls, v, values):
@@ -240,52 +172,55 @@ class RunShellActions(RunActions):
     def _run(cls, v, values):
         return functools.partial(run_shell, app=values["app"])
 
-    
 
 
 # %%
-
-class RunApp:
+class RunApp(widgets.HBox):
     def __init__(self,
                  config: typing.Any,
                  cls_ui: typing.Type[widgets.Box] = RunUi,
-                 cls_runactions: typing.Type[BaseModel]=RunShellActions
+                 cls_actions: typing.Type[BaseModel] = RunShellActions
                 ):
         """
         The goal of RunApp is to simplify the process of making a functional UI that interacts
         with remote data for use in a Jupyter Notebook or Voila App. 
-        
+
         Args:
             config: typing.Any
             cls_ui
             fn_buildactions
         """
-        self.ui_box = cls_ui(run_actions=RunActions(), name=config.pretty_name) # init with defaults
-        self.cls_runactions = cls_runactions
-        self.ui = self.ui_box.ui
-        self.config = config # the setter updates the ui.actions using fn_buildactions. can be updated on the fly
+        super().__init__(layout=widgets.Layout(flex="100%"))
+        self.children = [cls_ui(run_actions=RunActions(), name=config.pretty_name)]
+        self.ui = self.children[0].ui
+        #self.ui_box = cls_ui(run_actions=RunActions(), name=config.pretty_name) # init with defaults
+        self.cls_actions = cls_actions
+        self.config = config  # the setter updates the ui.actions using fn_buildactions. can be updated on the fly
         self._update_status()
-        
+
     @property
     def config(self):
         return self.ui.actions.config
-    
+
     @config.setter
     def config(self, value):
-        self.ui.actions = self.cls_runactions(config=value, app=self)
-        
+        self.ui.actions = self.cls_actions(config=value, app=self)
+        self.config.file(self.config.fpth_config)
+
+    @property
+    def actions(self):
+        return self.ui.actions
+
     def _update_status(self):
         st = get_status(self.config.fpths_inputs, self.config.fpths_outputs)
         self.ui.status = st
         self.config.status = st
-    
-    def display(self):
-        display(self.ui_box)
 
-    def _ipython_display_(self):
-        self.display()
-        
+#     def display(self):
+#         display(self.ui_box)
 
+#     def _ipython_display_(self):
+#         self.display()
 
 class MyConfigActionsShell(DefaultConfigActionsShell):
     @validator("fpths_outputs", always=True)
@@ -294,20 +229,196 @@ class MyConfigActionsShell(DefaultConfigActionsShell):
         nm = values['process_name']
         paths = [fdir / ('output-'+nm+'.csv'), fdir / ('out-' + nm + '.plotly.json')]
         return paths
-    
-PATH_EXAMPLE_SCRIPT = list(test_constants.DIR_EXAMPLE_PROCESS.glob('script*'))[0]
-MyConfigActionsShell = functools.partial(MyConfigActionsShell, fpth_script=PATH_EXAMPLE_SCRIPT, 
-                            displayfile_definitions=[
-                                DisplayfileDefinition(
-                                    path=PATH_EXAMPLE_SCRIPT.parent / 'schemas.py',
-                                    obj_name='LineGraph',
-                                    ext='.lg.json',
-                                    ftype=FiletypeEnum.input
-                                )])
 
-config = MyConfigActionsShell()
-run_app = RunApp(config)
-display(run_app)
+if __name__ == "__main__":
+    PATH_EXAMPLE_SCRIPT = list(test_constants.DIR_EXAMPLE_PROCESS.glob('script*'))[0]
+    MyConfigActionsShell = functools.partial(MyConfigActionsShell, fpth_script=PATH_EXAMPLE_SCRIPT, 
+                                displayfile_definitions=[
+                                    DisplayfileDefinition(
+                                        path=PATH_EXAMPLE_SCRIPT.parent / 'schemas.py',
+                                        obj_name='LineGraph',
+                                        ext='.lg.json',
+                                        ftype=FiletypeEnum.input
+                                    )])
+
+    config = MyConfigActionsShell()
+    run_app = RunApp(config)
+    display(run_app)
+
+# %%
+from ipyrun.constants import FNM_CONFIG_FILE
+from ipyrun.basemodel import BaseModel
+class ConfigBatch(BaseModel):
+    fdir_root: pathlib.Path
+    fpth_config: pathlib.Path = Field(default=FNM_CONFIG_FILE, description='name of config file for batch app')
+    title: str = Field(default='', description='markdown description of BatchApp')
+    cls_ui: Callable = Field(default=RunUi, description="the class that defines the RunUi widget container", exclude=True)
+    cls_actions: Callable = Field(default=RunShellActions, description="the class that defines the RunActions (extended with validators based on use case)", exclude=True)
+    cls_app: Union[Type, Callable] = Field(default=RunApp, description="the class that defines the RunApp.", exclude=True) # functools.partial used to define the cls_ui and cls_actions beofre passing here
+    cls_config: Union[Type, Callable] = Field(default=DefaultConfigActionsShell, description="the class that defines the config of a RunApp. this has can have fpth_script baked in", exclude=True)
+    configs: List[Type[BaseModel]] = []
+    runs: List[Callable] = Field(default=lambda: [], description="a list of RunApps", exclude=True)
+
+    @validator("fpth_config", always=True, pre=True)
+    def _fpth_config(cls, v, values):
+        """bundles RunApp up as a single argument callable"""
+        return values['fdir_root'] / v
+
+    @validator("cls_app", always=True, pre=True)
+    def _cls_app(cls, v, values):
+        """bundles RunApp up as a single argument callable"""
+        return functools.partial(v, cls_ui=values['cls_ui'], cls_actions=values['cls_actions'])
+
+
+
+# %%
+from ipyrun.ui_remove import RemoveRun
+
+def fn_add(app, cls_runapp, cls_config, **kwargs):
+    if "index" not in kwargs:
+        kwargs['index'] = app.runs.length
+    kwargs['fdir_appdata'] = app.config.fdir_root
+    config = cls_config(**kwargs)
+    app.configs_append(config)
+    newapp = cls_runapp(config)
+    app.runs.add_row(new_key=config.key, item=newapp)
+
+def fn_add_show(app):
+    return AddRun(fn_add=app.ui.actions.add)
+
+def fn_remove(app=None, key=None):
+    if key is None:
+        print('key is None')
+        key = app.runs.iterable[-1].key
+    fdir = [i.fdir_appdata for i in app.config.configs if i.key==key][0]
+    app.configs_remove(key)
+    shutil.rmtree(fdir)
+
+def fn_remove_show(app=None):
+    app.runs.add_remove_controls = 'remove_only'
+    return RemoveRun(app=app)
+
+def fn_remove_hide(app=None):
+    app.runs.add_remove_controls = None
+
+
+class BatchShellActions(BatchActions):
+    @validator("add", always=True)
+    def _add(cls, v, values):
+        return functools.partial(fn_add, values["app"], values["config"].cls_app, values["config"].cls_config)
+    
+    @validator("add_show", always=True)
+    def _add_show(cls, v, values):
+        return functools.partial(fn_add_show, values["app"])
+
+    @validator("remove", always=True)
+    def _remove(cls, v, values):
+        values["app"].runs.fn_remove = functools.partial(fn_remove, app=values["app"])
+        return values["app"].runs.remove_row
+    
+    @validator("remove_show", always=True)
+    def _remove_show(cls, v, values):
+        return functools.partial(fn_remove_show, values["app"])
+    
+    @validator("remove_hide", always=True)
+    def _remove_hide(cls, v, values):
+        return functools.partial(fn_remove_hide, values["app"])
+    
+    @validator("help_config_show", always=True)
+    def _help_config_show(cls, v, values):
+        return functools.partial(display_pydantic_json, values["config"], as_yaml=True)
+
+
+# %%
+class BatchApp(widgets.HBox):
+    def __init__(self,
+                 config: typing.Any,
+                 cls_ui: typing.Type[widgets.Box] = BatchUi,
+                 cls_actions: typing.Type[BaseModel] = BatchShellActions):
+        """
+        The goal of RunApp is to simplify the process of making a functional UI that interacts
+        with remote data for use in a Jupyter Notebook or Voila App. 
+
+        Args:
+            config: typing.Type[BaseModel]
+        """
+        super().__init__(layout=widgets.Layout(flex="100%")) #, flex="flex-grow"display="flex", 
+        self.children = [cls_ui(batch_actions=BatchActions(), title=config.title)]
+        self.ui = self.children[0].ui
+        self.cls_actions = cls_actions
+        self.config = config  # the setter updates the ui.actions using fn_buildactions. can be updated on the fly
+
+    @property
+    def runs(self):
+        return self.children[0].runs
+
+    @property
+    def config(self):
+        return self.ui.actions.config
+
+    @config.setter
+    def config(self, value):
+        self.ui.actions = self.cls_actions(config=value, app=self)
+        self.config.file(self.config.fpth_config)
+
+    def configs_append(self, config):
+        self.ui.actions.config.configs.append(config)
+        self.config.file(self.config.fpth_config)
+
+    def configs_remove(self, key):
+        self.config.configs = [c for c in self.config.configs if c.key != key]
+        self.config.file(self.config.fpth_config)
+
+    @property
+    def actions(self):
+        return self.ui.actions
+
+    def _update_status(self):
+        st = get_status(self.config.fpths_inputs, self.config.fpths_outputs)
+        self.ui.status = st
+        self.config.status = st
+# ----------------------------------------
+
+
+
+# %%
+config_batch = ConfigBatch(fdir_root=test_constants.DIR_EXAMPLE_BATCH, 
+                           cls_config=MyConfigActionsShell,
+                           title="""# Plot Straight Lines\n### example RunApp"""
+                          )
+app = BatchApp(config_batch)
+app
+
+# %%
+from ipyautoui.constants import BUTTON_MIN_SIZE
+BUTTON_MIN_SIZE
+
+# %%
+if __name__ == "__main__":
+    display(Markdown('change add remove to ToggleButtons?'))
+    display(widgets.HBox([
+        widgets.ToggleButtons(options = [('', 0), (' ', 1), ('  ', 2)], value=1,
+                  icons=['plus','','minus'],
+                  tooltips=['add a process','hide add / remove dialogue','remove a process'],
+                  style=widgets.ToggleButtonsStyle(button_width='20px'),
+                  layout=widgets.Layout(border='solid yellow')),
+        widgets.Button()]))
+
+# %%
+
+# %%
+
+# %%
+
+# %%
+
+# %%
+
+# %%
+
+# %%
+
+# %%
 
 # %%
 from typing import Union
@@ -332,7 +443,7 @@ class ConfigBatch(BaseModel):
     #fpth_script: pathlib.Path
     batch_desccription: str = ''
     run_config_model_definition: PyObj = None
-    run_config_model: Union[Type[BaseModel],Callable] = Field(None, exclude=True)
+    run_config_model: Union[Type[BaseModel], Callable] = Field(None, exclude=True)
     run_app_model_definition: PyObj = None
     run_app_model: Callable = Field(RunApp, exclude=True)
     runs: List[Run] = []
