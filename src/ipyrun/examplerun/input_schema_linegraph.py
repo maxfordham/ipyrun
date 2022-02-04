4ebd1252e1dded06c97d452e4a4603b9c845fd10
john <j.gunstone@maxfordham.com>
2022-02-04 17:38:29 +0000

    added examplerun script to main repo. added test constants

---
+++ src/ipyrun/examplerun/input_schema_linegraph.py
@@ -0,0 +1,27 @@
+from pydantic import BaseModel , Field, conint
+import typing
+import pathlib
+
+class BaseModel(BaseModel):
+    def file(self, path: pathlib.Path, **json_kwargs):
+        """
+        this is a method that is added to the pydantic BaseModel within AutoUi using 
+        "setattr". i.e. setattr(self.config_autoui.pydantic_model, 'file', file)
+        Args:
+            self (pydantic.BaseModel): instance
+            path (pathlib.Path): to write file to 
+        """
+        if 'indent' not in json_kwargs.keys():
+            json_kwargs.update({'indent':4})
+        path.write_text(self.json(**json_kwargs), encoding='utf-8')
+
+class LineGraph(BaseModel):
+    """parameters to define a simple `y=m*x + c` line graph"""
+    title: str = Field(default='line equation', description='add chart title here')
+    m: float = Field(default=2, description='gradient')
+    c: float = Field(default=5, description='intercept')
+    x_range: tuple[int, int] = Field(default=(0,5), ge=0, le=50, description='x-range for chart')
+    y_range: tuple[int, int] = Field(default=(0,5), ge=0, le=50, description='y-range for chart')
+        
+if __name__ == "__main__":
+    lg = LineGraph()
--- tests/constants.py
+++ tests/constants.py
@@ -2,5 +2,6 @@ import pathlib
 
 DIR_TESTS = pathlib.Path(__file__).parent
 DIR_EXAMPLE_PROCESS = DIR_TESTS / 'examples' / 'line_graph'
+FDIR_APPDATA = DIR_TESTS / 'examples' / 'fdir_appdata'
 PATH_EXAMPLE_SCRIPT = DIR_TESTS / 'examples' / 'script_line_graph.py'
 DIR_EXAMPLE_BATCH = DIR_TESTS / 'examples' / 'line_graph_batch'
