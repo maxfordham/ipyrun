aca12370981cf931b3a96fef4578670d153b1c5e
jgunstone <gunstone.john@gmail.com>
2022-12-15 14:07:10 +0000

    added test

--- tests/test_ipyrun.py
+++ tests/test_ipyrun.py
@@ -3,28 +3,28 @@
 """Tests for `ipyrun` package."""
 
 import unittest
-import ipyrun
 
-#from .constants import DIR_EXAMPLE_APP
+from .constants import DIR_EXAMPLE_APP, DIR_EXAMPLE_APPDATA
 
 import subprocess
-# TODO: add tests! 
+
+# TODO: add tests!
+from ipyrun.runshell import DefaultConfigShell, ConfigShell, run
+
 
 class TestIpyrun(unittest.TestCase):
     """Tests for `ipyrun` package."""
 
-    def setUp(self):
-        """Set up test fixtures, if any."""
-        print('sdf')
-
-    def tearDown(self):
-        """Tear down test fixtures, if any."""
-        pass
-        
     def test_running_app(self):
         """Test something."""
-        pass
-        #try: 
-        #    subprocess.run('python {}'.format(DIR_EXAMPLE_APP))
-        #except:
-        #    ValueError('failed to run')
+        config = ConfigShell(
+            path_run=DIR_EXAMPLE_APP,
+            fdir_root=DIR_EXAMPLE_APPDATA,
+            fpths_inputs=["in-linegraph.lg.json"],
+            fpths_outputs=["out-linegraph.csv", "out-linegraph.plotly.json"],
+            shell='python -O -m',
+            pythonpath=DIR_EXAMPLE_APP,
+        )
+        pr = run(config)
+        print("config")
+        assert isinstance(config, ConfigShell)
