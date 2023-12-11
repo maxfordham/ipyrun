b2a3073c97c5df08c0710c6d8d84cc042ef3b609
jgunstone <j.gunstone@maxfordham.com>
2023-12-11 20:23:45 +0000

    bump-pydantic. update check_validators

--- src/ipyrun/basemodel.py
+++ src/ipyrun/basemodel.py
@@ -1,20 +1,21 @@
 """Added functionality to default pydantic.BaseModel (mostly for reading and
 writing to file)
 """
-from pydantic import BaseModel, validate_model
+from pydantic import ConfigDict, BaseModel
 import stringcase
 import pathlib
 import subprocess
 from typing import Type
 
-def file(self:Type[BaseModel], path: pathlib.Path, **json_kwargs):
+
+def file(self: Type[BaseModel], path: pathlib.Path, **json_kwargs):
     """
     this is a method that is added to the pydantic BaseModel within AutoUi using
     "setattr".
-    
+
     Example:
         ```setattr(model, 'file', file)```
-        
+
     Args:
         self (pydantic.BaseModel): instance
         path (pathlib.Path): to write file to
@@ -25,7 +26,8 @@ def file(self:Type[BaseModel], path: pathlib.Path, **json_kwargs):
         json_kwargs.update({"indent": 4})
     path.write_text(self.json(**json_kwargs), encoding="utf-8")
 
-def file_schema(self:Type[BaseModel], path: pathlib.Path, **json_kwargs):
+
+def file_schema(self: Type[BaseModel], path: pathlib.Path, **json_kwargs):
     if type(path) == str:
         path = pathlib.Path(path)
     path = path.with_suffix(".schema.json")
@@ -33,8 +35,9 @@ def file_schema(self:Type[BaseModel], path: pathlib.Path, **json_kwargs):
         json_kwargs.update({"indent": 4})
     path.write_text(self.schema(**{"indent": 4}), encoding="utf-8")
 
-def file_mdschema(self:Type[BaseModel], path: pathlib.Path, **json_kwargs):
-    """creates markdown file from jsonschema using 
+
+def file_mdschema(self: Type[BaseModel], path: pathlib.Path, **json_kwargs):
+    """creates markdown file from jsonschema using
 
     Reference:
         https://github.com/RalfG/jsonschema2md
@@ -47,31 +50,25 @@ def file_mdschema(self:Type[BaseModel], path: pathlib.Path, **json_kwargs):
     path_schema = path.with_suffix(".schema.json")
     self.file_schema(path_schema, **json_kwargs)
     subprocess.run(["jsonschema2md", str(path_schema), str(path)])
-    
-def check_validators(self:Type[BaseModel]):
-    values, fields_set, validation_error = validate_model(
-        self.__class__, self.__dict__
-    )
-    if validation_error:
-        raise validation_error
+
+
+def check_validators(self: Type[BaseModel]):
+    model = self.model_validate(self.model_dump())
+
     try:
-        object.__setattr__(self, "__dict__", values)
+        setattr(self, "__dict__", model.model_dump())
     except TypeError as e:
         raise TypeError(
             "Model values must be a dict; you may not have returned "
             + "a dictionary from a root validator"
         ) from e
-    object.__setattr__(self, "__fields_set__", fields_set)
+
 
 class BaseModel(BaseModel):
-
-    class Config:
-        #alias_generator = stringcase.titlecase
-        allow_population_by_field_name = True
-        arbitrary_types_allowed = True
+    model_config = ConfigDict(populate_by_name=True, arbitrary_types_allowed=True)
 
 
-setattr(BaseModel, 'file', file)
-setattr(BaseModel, 'file_schema', file_schema)
-setattr(BaseModel, 'file_mdschema', file_mdschema)
-setattr(BaseModel, 'check_validators', check_validators)
+setattr(BaseModel, "file", file)
+setattr(BaseModel, "file_schema", file_schema)
+setattr(BaseModel, "file_mdschema", file_mdschema)
+setattr(BaseModel, "check_validators", check_validators)
